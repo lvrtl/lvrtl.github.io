ad5f035d2e08aa3d16f03f0218d0164518e97559
Laura Rintoul <lrintoul@protonmail.com>
2024-06-02 15:47:18 +0100

    a tale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { TalesComponent } from './tales/tales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    TalesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tales/tales.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tales',
+  templateUrl: './tales.component.html',
+  styleUrls: ['./tales.component.scss']
+})
+export class TalesComponent {
+
+}
